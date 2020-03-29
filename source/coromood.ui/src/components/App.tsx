1776509e14b3ca84dc16c273437550409ac70e06
Ron <rieck@landaumedia.de>
2020-03-29 12:02:25 +0200

    Init UI with create-react-app (typescript)

---
+++ source/coromood.ui/src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import "../styles/App.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Coromood</p>
+        <h3>WIP</h3>
+      </header>
+    </div>
+  );
+};
